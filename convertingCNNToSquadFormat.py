40809859519c8c0ee2062d7b7e72eadef6ca635f
Ravneet Kaur <47275109+ravneetKaur2159@users.noreply.github.com>
2019-03-01 12:46:21 +0800

    answer function impl

--- convertingCNNToSquadFormat.py
+++ convertingCNNToSquadFormat.py
@@ -2,9 +2,32 @@
 import csv
 import os as os
 import pandas as pd
+import re as regex
 
 filePathDataset = os.path.abspath("../../Masters Projects/Dataset/newsqa-data-v1/newsqa-data-v1Copy.csv")
-filePathStories = os.path.abspath("../../Masters Projects/Dataset/cnn_stories/cnn_stories/cnn/stories")
+filePathStories = os.path.abspath("../../Masters Projects/Dataset/cnn_stories/cnn_stories")
+REPLACE_WITH_NO_SPACE = \
+    regex.compile("(\()|(\,)|(\")|(\))|(\–)|(\.)|(\;)|(\!)|(\-)|(<br />)|@highlight|(cnn)|(\:)|(\“)|(\’)|(\‘)|(\”)|(\')")
+
+# Function does first-level data cleaning
+def getStoryPreProcessedContent(storyContent):
+    content = ""
+    for line in storyContent:
+        content = content + (regex.sub(REPLACE_WITH_NO_SPACE, "", line).lower()).strip()
+
+    return content
+
+# Function determines if at all an answer is present
+def isAnswerPresent(answerArray):
+    for i in len(answerArray):
+        if answerArray[i].lower().strip() != "none":
+            return answerArray[i]
+
+    return False
+
+# Function returns the answer of the question
+def getAnswerGivenCharRange(ansCharRange):
+
 
 # Initializing
 dataFrameDataSet = pd.read_csv(filePathDataset)
@@ -20,18 +43,53 @@ qasElement = {}
 dataElement["paragraphs"] = [] # Array of objects
 paragraphElement = {}
 dataElement["qas"] = [] # Array of objects
+
+storiesId = {}
 # skip question if no answer
 
-dataElement["title"] = "someDummyTitle"
 
 # Construction of JSON data
-# Stories Path
-dataFrameDataSet
 
 
-dataElement["context"] = "fsdfsdfsdgfdgiojaguiohtuhrtwerhrhwearj"
+for i in len(dataFrameDataSet):
+    # Skip if no answer present
+    answerArray = (dataFrameDataSet["answer_char_ranges"][i]).split("|")
+    answerPresence = isAnswerPresent(answerArray)
+
+    if not answerPresence:
+        # No answer present
+        continue
+
+    # Get answer
+    answer = getAnswerGivenCharRange(answerPresence)
+
+    storyId = dataFrameDataSet["story_id"][i]
+
+
+    if(storiesId[storyId]):
+        # paragraph already present, question has been added
+    else:
+        # new paragraph added
+        storiesId[storyId] = True
+        dataElement["title"] = "someDummyTitle"
+
+        # Stories Path
+        storiesPath = os.path.abspath(filePathStories + dataFrameDataSet["story_id"][0])
+        if not os.path.isfile(storiesPath):
+            raise TypeError(storiesPath + " is not present")
+
+        story = open(storiesPath, encoding="utf-8")
+
+
+
+        dataElement["context"] = getStoryPreProcessedContent(story)
+
+print(dataFrameDataSet["story_id"][0])
+
+
 answerElement["answer_start"] = 654
 answerElement["text"] = "some answer"
+
 
 dataElement["question"] = "To whom did the Virgin Mary allegedly appear in 1858 in Lourdes France?"
 dataElement["id"] = "SomeUniqueId"
@@ -54,4 +112,4 @@ data["data"].append(dataObject)
 
 squadWrapper["data"] = data["data"]
 squadWrapper["version"] = "1.1"
-print(squadWrapper)
+
