188dd51c404f7f72db58fd97e09489d6dae51811
Ayush Gera <ayushgera@gmail.com>
2019-04-15 16:28:09 +0800

    runs cript error

--- BERT/run_reBERT.sh
+++ BERT/run_reBERT.sh
@@ -1,23 +1,22 @@
-python run_reBERT.py --bert_model experiment/squad_newsqa_synonym_0.5/ \
+python run_reBERT.py --bert_model bert-base-uncased \
 --do_train \
 --do_lower_case \
---train_file data/newsqa/newsqa_singleSynonymQuestions_0.5.json \
+--train_file data/newsqa/newsqa_validated_90_91.json \
 --learning_rate 0.0005 \
---vocab_file data/newsqa/vocab.txt \
 --num_train_epochs 1 \
 --max_seq_length 192 \
 --max_query_length 32 \
 --warmup_proportion 0.2 \
 --max_answer_length 15 \
 --train_batch_size 64 \
---output_dir experiment/squad_newsqa_synonym_0.5
+--output_dir experiment/reBERT
 
-python run_reBERT.py --bert_model experiment/squad_newsqa_synonym_0.5 \
+python run_reBERT.py --bert_model bert-base-uncased \
  --do_predict --do_lower_case --predict_file data/newsqa/test.json \
- --output_dir experiment/squad_newsqa_synonym_0.5
+ --output_dir experiment/reBERT
 
 python evaluate-reBERT.py \
 data/newsqa/test.json \
-experiment/squad_newsqa_synonym_0.5/predictions.json \
-experiment/squad_newsqa_synonym_0.5/eval_result
+experiment/reBERT/predictions.json \
+experiment/reBERT/eval_result
 
